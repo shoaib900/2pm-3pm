f0982101d7579b1466f2a52da011f1936dea8d2f
shoaib900 <rshoaib376@gmail.com>
2023-08-10 14:50:04 +0500

    2pm to 3 pm

---
+++ script.js
@@ -0,0 +1,74 @@
+var data = "eadfkasjd"
+// var num = 45;
+// num = num + 45
+// num += 45;
+// num = (3* 5) +(10 /5);
+// console.log(num)
+
+// var name = "shoaib";
+// var num = 54
+
+// console.log(name + " 56 " + num);
+// console.log(`${name} ${num}`)
+
+// var x = Number(prompt( " what is your age"))
+// if( x <= 20 && x >= 11){
+//  console.log("your answer is right")
+// }else if(x === "25"){
+//     console.log(`value is ${x}`)
+// }
+
+// var x = Number(prompt("age"))
+// if (x >= 25){
+//     var y = Number(prompt("height"))
+//     if(y === 30){
+//         console.log("you are highered")
+//     }else{
+//         console.log("sorry")
+//     }
+// }else{
+//     console.log("negosiate")
+// }
+
+//////////////////////////////////////////////////////////////////////
+
+// var arr = [1,0,45,34,34,43,32,12]
+// var arr2 =["hello","world"]
+// var arr3 =[43,"hello hony banny"]
+// console.log(arr.length);
+// var string = arr.toString()
+// console.log(string)
+// console.log(arr.pop())
+// console.log(arr.push("hello world"))
+// console.log(arr.unshift("hello world","you"))
+// console.log(arr.shift())
+// console.log(arr.join(" - "))
+// delete arr[0]
+// arr[1] = "hello Mr. Shoaib"
+// console.log(arr)
+// console.log(arr.concat(arr2,arr3))
+// Array filter()
+// var even = arr.filter(n => n % 2 == 0)
+// console.log(even)
+// Array splice()
+// arr.splice(2,4,"hello mania","hi")
+// console.log(arr);
+// Array slice()
+// var arr0 =[1,2,3,4,5]
+// var arr = arr0.slice(0,5)
+// console.log(arr);
+
+var x = Number(prompt("enter the number"))
+
+var y =Math.round((Math.random(5) * 10))
+if(x == y){
+    console.log("your value is same")
+}else{
+    console.log("your value is not same")
+}
+
+
+
+
+
+
